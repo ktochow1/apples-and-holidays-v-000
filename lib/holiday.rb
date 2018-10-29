27b4427b36a8a78b054e7c9ea60975184af84a08
ktochow1 <ktochow1@binghamton.edu>
2018-10-29 22:12:20 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -94,6 +94,7 @@ def all_holidays_with_bbq(holiday_hash)
         
         holiday_hash.map do |season, holidays|
           holidays.map do |holiday_name, supplies|
-            supplies.include?("BBQ") ? 
+            supplies.include?("BBQ") ? holiday_name : nil 
+            
           
 end
